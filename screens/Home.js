a109c5350343419361cb45a024180559d2bb71e4
taiki <t7686taiki@gmail.com>
2018-10-21 09:46:10 +0900

    レイアウトを調整

--- screens/Home.js
+++ screens/Home.js
@@ -8,25 +8,26 @@ import {
   RefreshControl,
   Dimensions,
   Image,
+  ImageBackground,
 } from 'react-native';
 import axios from 'axios';
 import DataMain from './DataMain';
 
-const LabelBox = (props) => {
-  return (
-    <View style={styles.labelBox}>
-      <Text style={styles.labelText}>{props.label}</Text>
-    </View>
-  )
-}
-
-const TotalLabelBox = (props) => {
-  return (
-    <View style={styles.labelBox}>
-      <Text style={styles.totallabelText}>{props.label}</Text>
-    </View>
-  )
-}
+// const LabelBox = (props) => {
+//   return (
+//     <View style={styles.labelBox}>
+//       <Text style={styles.labelText}>{props.label}</Text>
+//     </View>
+//   )
+// }
+//
+// const TotalLabelBox = (props) => {
+//   return (
+//     <View style={styles.labelBox}>
+//       <Text style={styles.totallabelText}>{props.label}</Text>
+//     </View>
+//   )
+// }
 
 class Home extends Component {
   constructor(props) {
@@ -47,7 +48,7 @@ class Home extends Component {
     .then(()=>{
       setTimeout( () => {
         this.setState({refreshing2: false})
-      }, 3000)
+      }, 1000)
     });
   }
 
@@ -114,50 +115,87 @@ class Home extends Component {
 
               <View style={styles.box_bottom}>
                 <View style={styles.scoreBox}>
-                  <View style={styles.box_bottom_over_top}>
-                    <View
-                      style={[styles.eachscoreBox, {marginLeft: 80}, {backgroundColor: '#9DDCDC'}]}
-                      >
-                      <LabelBox label={'きれいさ'} />
-                      <View style={styles.eachscoredisplay}>
-                        <Text style={styles.scoreText}>{inputdata.id}</Text>
-                      </View>
+                  <View style={styles.box_bottom_score}>
+                    <ImageBackground
+                      source={require("../assets/images/colorbar_2.png")}
+                      style={styles.kirakira}>
+                      <View style={styles.iconBox}>
+                        <Image
+                          source={require("../assets/images/kirakira.png")}
+                          style={styles.imgScreen}
+                          />
+                          </View>
+
+                        <View style={styles.labelBox}>
+                          <Text style={styles.labelText}>{'きれいさ'}</Text>
+                        </View>
+
+                        <View style={styles.eachscoreBox}>
+                          <Text style={styles.scoreText}>{inputdata.id}</Text>
+                        </View>
+                    </ImageBackground>
+                  </View>
+
+
+                  <View style={styles.box_bottom_score}>
+                    <View style={styles.iconBox}>
+                      <Image
+                        source={require("../assets/images/nose.png")}
+                        style={styles.imgScreen}
+                      />
+                    </View>
+
+                    <View style={styles.labelBox}>
+                      <Text style={styles.labelText}>{'におい'}</Text>
+                    </View>
+
+                    <View style={styles.eachscoreBox}>
+                      <Text style={styles.scoreText}>{inputdata.id}</Text>
                     </View>
                   </View>
-                  <View style={styles.box_bottom_over_bot}>
-                    <View
-                      style={[styles.eachscoreBox, {marginLeft: 25}, {backgroundColor: '#F0E68C'}]}
-                      >
-                      <LabelBox label={'におい'} />
-                      <View style={styles.eachscoredisplay}>
-                        <Text style={styles.scoreText}>{inputdata.id}</Text>
-                      </View>
+
+                  <View style={styles.box_bottom_score}>
+                    <View style={styles.iconBox}>
+                      <Image
+                        source={require("../assets/images/dust.png")}
+                        style={styles.imgScreen}
+                      />
                     </View>
-                    <View style={[styles.eachscoreBox,{marginLeft: 6}, {backgroundColor: '#E67A7A'}]}>
-                      <LabelBox label={'ほこり'} />
-                      <View style={styles.eachscoredisplay}>
-                        <Text style={styles.scoreText}>{inputdata.id}</Text>
-                      </View>
+
+                    <View style={styles.labelBox}>
+                      <Text style={styles.labelText}>{'ほこり'}</Text>
+                    </View>
+
+                    <View style={styles.eachscoreBox}>
+                      <Text style={styles.scoreText}>{inputdata.id}</Text>
                     </View>
                   </View>
                 </View>
 
-                <View style={styles.scoreBox_middle} />
+                <View style={styles.middlescoreBox} />
 
                 <View style={styles.totalscoreBox}>
-                  <TotalLabelBox label={'とくてん'} />
+                  <View style={styles.totallabelBox}>
+                    <Text style={styles.totallabelText}>{'とくてん'}</Text>
+                  </View>
+
                   <View style={styles.totalscoredisplay}>
                     <Text style={styles.totalscoreText}>{inputdata.id}</Text>
                   </View>
+
                 </View>
-              </View>
+
             </View>
+          </View>
 
           </ScrollView>
-                );
+            );
         }
   }
 }
+
+
+
 
 const styles = StyleSheet.create({
   anime: {
@@ -178,7 +216,7 @@ const styles = StyleSheet.create({
   },
   box_top: {
     flex: 7,
-    borderRadius: 30,
+    //borderRadius: 30,
   },
   box_middle: {
     flex: 0.1,
@@ -187,47 +225,64 @@ const styles = StyleSheet.create({
     flex: 4,
     flexDirection: 'row',
   },
-  box_bottom_over_top: {
-    flex: 1,
-  },
-  box_bottom_over_bot: {
-    flex: 1.1,
-    flexDirection: 'row',
-  },
   scoreBox: {
     flex: 2,
-    // borderWidth: 1,
-    borderRadius: 30,
-    backgroundColor: '#FFF4E1',
+    borderWidth: 1,
+    //borderRadius: 30,
+    //backgroundColor: '#FFF4E1',
+    flexDirection: 'column',
   },
-  scoreBox_middle : {
+  middlescoreBox: {
     flex: 0.03,
   },
   totalscoreBox: {
     flex: 1,
-    //borderWidth: 1,
-    borderRadius: 30,
+    borderWidth: 1,
+    //borderRadius: 30,
     backgroundColor: "#FFF4E1",
+    //flexDirection: 'row',
   },
-  eachscoreBox: {
-    borderWidth: 2,
-    borderBottomLeftRadius: 100,
-    borderBottomRightRadius: 100,
-    borderTopLeftRadius: 100,
-    borderTopRightRadius: 100,
-    height: Dimensions.get('window').height / 6.6,
-    width: Dimensions.get('window').height / 6.6,
-    borderColor: 'black',
+  box_bottom_score: {
+    flex: 1,
+    //borderWidth: 1,
+    flexDirection: 'row',
+  },
+  iconBox: {
+    flex: 1,
+    borderBottomWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imgScreen: {
+    flex: 1,
+    width: 60,
+    height: 20,
   },
   labelBox: {
     flex: 2,
     alignItems: 'center',
     justifyContent: 'center',
+    borderBottomWidth: 1,
+  },
+  eachscoreBox: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    // borderBottomLeftRadius: 100,
+    // borderBottomRightRadius: 100,
+    // borderTopLeftRadius: 100,
+    // borderTopRightRadius: 100,
+    borderBottomWidth: 1,
+  },
+  kirakira: {
     width: '100%',
     height: '100%',
+    flexDirection: 'row',
+    //resizeMode: 'cover',
+    //backgroundColor: 'transparent',
   },
-  eachscoredisplay: {
-    flex: 3,
+  totallabelBox: {
+    flex: 2,
     alignItems: 'center',
     justifyContent: 'center',
   },
